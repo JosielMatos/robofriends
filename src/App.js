16eaad2f253a97c30d5210f256c8e7bf8b116f3d
JosielMatos <josielmatos77@protonmail.com>
2021-09-17 15:33:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Card from './components/Card';
+import { robots } from './robots'
+import 'tachyons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {robots.map(robot => <Card id={robot.id} name={robot.name} email={robot.email}/>)}      
     </div>
   );
 }
 
 export default App;
+
