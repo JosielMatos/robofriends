84d210d7480db6b2302d6e02f06ce92861ccce98
JosielMatos <josielmatos77@protonmail.com>
2021-09-25 11:39:31 -0300

    Add searchboxx rounded corners

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,7 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <>
       <input
-        className='pa3 ba b--green bg-lightest-blue'
+        className='pa3 ba b--green br3 bg-lightest-blue'
         type='search'
         placeholder='Search robots'
         onChange={searchChange}
