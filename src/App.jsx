0b4763563a5c5b1f4a9cdf2cd13d4e230190b139
JosielMatos <josielmatos77@protonmail.com>
2021-09-25 11:36:32 -0300

    Add api fetch, usestate, loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import './App.css';
-import { useState } from 'react';
-import { robots } from './robots'
-import SearchBox from './components/SearchBox';
-import CardsList from './components/CardsList';
-import 'tachyons';
+import "./App.css";
+import { useEffect, useState } from "react";
+import SearchBox from "./components/SearchBox";
+import CardsList from "./components/CardsList";
+import "tachyons";
 
 function App() {
+  const [robots, setRobots] = useState([]);
   const [searchField, setSearchField] = useState("");
-  const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()))
+  const filteredRobots = robots.filter((robot) =>
+    robot.name.toLowerCase().includes(searchField.toLowerCase())
+  );
 
   function onSearchChange(event) {
-      setSearchField(event.target.value);
+    setSearchField(event.target.value);
   }
 
-  return (
-    <main className="App">
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => setRobots(users));
+  });
+
+  return !robots.length ? (
+    <h1 className='tc'>Loading...</h1>
+  ) : (
+    <main className='App'>
       <header>
-        <h1 className="f1">RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={onSearchChange} />
       </header>
       <CardsList robots={filteredRobots} />
